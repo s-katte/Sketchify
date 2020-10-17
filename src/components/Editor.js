5603286fef0b172b11288151808b8b269960340f
s-katte <s-katte@users.noreply.github.com>
2020-10-17 11:23:06 +0000

    Apply formatting changes

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -9,29 +9,27 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCompressAlt, faExpandAlt } from "@fortawesome/free-solid-svg-icons";
 
 export default function Editor(props) {
-  const { language, displayName, value, onChange } = props;
+    const { language, displayName, value, onChange } = props;
 
-  function handleChange(editor, data, value) {
-    onChange(value);
-  }
+    function handleChange(editor, data, value) {
+        onChange(value);
+    }
 
-  return (
-    <div className="editor-container">
-      <div className="editor-title">
-        {displayName}
-      </div>
-      <ControlledEditor
-        onBeforeChange={handleChange}
-        value={value}
-        className="code-mirror-wrapper"
-        options={{
-          lineWrapping: true,
-          lint: true,
-          mode: language,
-          theme: "material",
-          lineNumbers: true,
-        }}
-      />
-    </div>
-  );
+    return (
+        <div className="editor-container">
+            <div className="editor-title">{displayName}</div>
+            <ControlledEditor
+                onBeforeChange={handleChange}
+                value={value}
+                className="code-mirror-wrapper"
+                options={{
+                    lineWrapping: true,
+                    lint: true,
+                    mode: language,
+                    theme: "material",
+                    lineNumbers: true,
+                }}
+            />
+        </div>
+    );
 }
--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -3,22 +3,22 @@ import { useEffect, useState } from "react";
 const PREFIX = "codepen-clone-";
 
 export default function useLocalStorage(key, initialValue) {
-  const prefixedKey = PREFIX + key;
+    const prefixedKey = PREFIX + key;
 
-  const [value, setValue] = useState(() => {
-    const jsonValue = localStorage.getItem(prefixedKey);
-    if (jsonValue != null) return JSON.parse(jsonValue);
+    const [value, setValue] = useState(() => {
+        const jsonValue = localStorage.getItem(prefixedKey);
+        if (jsonValue != null) return JSON.parse(jsonValue);
 
-    if (typeof initialValue === "function") {
-      return initialValue();
-    } else {
-      return initialValue;
-    }
-  });
+        if (typeof initialValue === "function") {
+            return initialValue();
+        } else {
+            return initialValue;
+        }
+    });
 
-  useEffect(() => {
-    localStorage.setItem(prefixedKey, JSON.stringify(value));
-  }, [prefixedKey, value]);
+    useEffect(() => {
+        localStorage.setItem(prefixedKey, JSON.stringify(value));
+    }, [prefixedKey, value]);
 
-  return [value, setValue];
+    return [value, setValue];
 }
